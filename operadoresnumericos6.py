dd29b63ac1d278992888242bd1fa21a984d0751f
juancampos45 <55142610+juancampos45@users.noreply.github.com>
2019-09-12 10:39:23 -0400

    Add files via upload

---
+++ operadoresnumericos6.py
@@ -0,0 +1,21 @@
+# Proposito: Trabajar con los operadores aritmrtico
+input('Suponga que a=49 b=5 c=3, pulse cualquier tecla para realizar el siguiente programa: ')
+input('A % (B * C) + 1, pulse cualquier tecla para obtener su resultado')
+# Entrada de datos
+
+a = 49
+
+b = 5
+
+c = 3
+
+# Procedimiento
+z = a % (b * c) + 1
+
+# Salida del programa
+
+print('El resultado del programa es: ',z)
+
+# Fin del programa
+
+input(' pulse cualquier tecla para continuar...')# pausa
